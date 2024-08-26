a2e42e76e33d0905baa3fd45b5f4c4eebcc5047a
KaminskyEugeny <71308216+KaminskyEugeny@users.noreply.github.com>
2024-08-26 14:56:00 +0300

    Add files via upload

---
+++ Crickets.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    void Print(string text = "crickets")
+    {
+        Debug.Log(text);  // Выводим в консоль текст "crickets"
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Print();   
+    }
+}
---
+++ Destroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Destroy(gameObject); // Удаляем объект - хозяина скрипта до первого карда
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
